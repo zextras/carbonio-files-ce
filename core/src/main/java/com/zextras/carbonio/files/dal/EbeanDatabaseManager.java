c7d9766071abcc47739f036957af8c98dd82a682
Federico Rispo <federico.rispo@zextras.com>
2022-04-13 12:24:09 +0200

    chore: remove db password

--- core/src/main/java/com/zextras/carbonio/files/dal/EbeanDatabaseManager.java
+++ core/src/main/java/com/zextras/carbonio/files/dal/EbeanDatabaseManager.java
@@ -76,7 +76,7 @@ public class EbeanDatabaseManager {
     postgresPassword = ServiceDiscoverHttpClient
       .defaultURL("carbonio-files")
       .getConfig("db-password")
-      .getOrElse("38ce42d9a2b2d2734303359fb55555e2");
+      .getOrElse("");
 
     jdbcPostgresUrl = "jdbc:postgresql://"
       + config.getProperty(Files.Config.Database.URL, "127.78.0.2")
