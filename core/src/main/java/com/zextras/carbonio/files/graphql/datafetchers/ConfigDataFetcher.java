c60dabc76e04fc2352ce3d352c9d459adaaff6c9
Daniele Giachetto <daniele.giachetto@zextras.com>
2022-05-10 15:28:03 +0200

    feat: expose max_number_of_keep_version
    Co-authored-by: Federico Rispo <federico.rispo@zextras.com>

--- core/src/main/java/com/zextras/carbonio/files/graphql/datafetchers/ConfigDataFetcher.java
+++ core/src/main/java/com/zextras/carbonio/files/graphql/datafetchers/ConfigDataFetcher.java
@@ -2,8 +2,8 @@ package com.zextras.carbonio.files.graphql.datafetchers;
 
 import com.google.inject.Inject;
 import com.zextras.carbonio.files.Files;
-import com.zextras.carbonio.files.Files.Consul;
-import com.zextras.carbonio.files.Files.Consul.Config;
+import com.zextras.carbonio.files.Files.ServiceDiscover;
+import com.zextras.carbonio.files.Files.ServiceDiscover.Config;
 import com.zextras.carbonio.files.Files.GraphQL;
 import com.zextras.carbonio.files.clients.ServiceDiscoverHttpClient;
 import com.zextras.carbonio.files.graphql.GraphQLProvider;
@@ -18,24 +18,26 @@ import java.util.concurrent.CompletableFuture;
 /**
  * <p>Contains all the implementations of {@link DataFetcher}s for all the queries and mutations
  * defined in the GraphQL schema that are related to the {@link Files.GraphQL.Config} type.</p>
- * <p>Each {@link DataFetcher} implementation is asynchronous and returns a {@link List} of {@link HashMap}
- * containing the data fetched from consul.</p>
+ * <p>Each {@link DataFetcher} implementation is asynchronous and returns a {@link List} of
+ * {@link HashMap} containing the data fetched from service discover.</p>
  * <p>These {@link DataFetcher}s will be used in the {@link GraphQLProvider} where they are bound
  * with the related queries, mutations and composed attributes.</p>
  */
 public class ConfigDataFetcher {
 
   private final Map<String, String> configMap;
+  private       String              maxKeepVersionsValue;
 
   /**
    * <p> This constructor initializes the map of config keys which values will be
-   * requested at consul.
-   * The value is initialized at the default value of every specific configuration</p>
+   * requested at service discover. The value is initialized at the default value of every specific
+   * configuration</p>
    */
   @Inject
   public ConfigDataFetcher() {
     configMap = new HashMap<>();
-    configMap.put(Config.MAX_VERSIONS, Config.DEFAULT_MAX_VERSIONS);
+    configMap.put(Config.MAX_VERSIONS, String.valueOf(Config.DEFAULT_MAX_VERSIONS));
+    maxKeepVersionsValue = String.valueOf(Config.DEFAULT_MAX_KEEP_VERSIONS);
   }
 
   /**
@@ -43,34 +45,49 @@ public class ConfigDataFetcher {
    * query.</p>
    * <p>The request does not need any parameters in input.</p>
    * <h2>Behaviour:</h2>
-   * <p>It fetches the configs from consul, if one of the configs is not found it will return the
-   * default value for that config. </p>
+   * <p>It fetches the configs from service discover, if one of the configs is not found it will
+   * return the default value for that config. </p>
    *
-   * @return an asynchronous {@link DataFetcher} containing a {@link List} with a
-   * {@link  Map} in every entry.
+   * @return an asynchronous {@link DataFetcher} containing a {@link List} with a {@link  Map} in
+   * every entry.
    */
   public DataFetcher<CompletableFuture<List<DataFetcherResult<Map<String, String>>>>> getConfigs() {
     return environment -> CompletableFuture.supplyAsync(() -> {
       List<DataFetcherResult<Map<String, String>>> result = new ArrayList<>();
       configMap.forEach((key, value) -> {
-        Map<String, String> resultMap = new HashMap<>();
-        resultMap.put(
-          GraphQL.Config.NAME,
-          key
-        );
-        resultMap.put(
-          GraphQL.Config.VALUE,
-          ServiceDiscoverHttpClient
-            .defaultURL(Consul.SERVICE_NAME)
-            .getConfig(key)
-            .getOrElse(value)
-        );
-        result.add(DataFetcherResult
-          .<Map<String, String>>newResult()
-          .data(resultMap)
-          .build());
+        String currValue = ServiceDiscoverHttpClient
+          .defaultURL(ServiceDiscover.SERVICE_NAME)
+          .getConfig(key)
+          .getOrElse(value);
+        result.add(convertConfigToGraphQLMap(key, currValue));
       });
+      result.add(convertConfigToGraphQLMap(Config.MAX_KEEP_VERSIONS, maxKeepVersionsValue));
       return result;
     });
+  }
+
+  private DataFetcherResult<Map<String, String>> convertConfigToGraphQLMap(
+    String key,
+    String value
+  ) {
+    Map<String, String> resultMap = new HashMap<>();
+    resultMap.put(
+      GraphQL.Config.NAME,
+      key
+    );
+    resultMap.put(
+      GraphQL.Config.VALUE,
+      value
+    );
+    // TODO: Federico, FIX ME PLEASE
+    if (Config.MAX_VERSIONS.equals(key)) {
+      maxKeepVersionsValue = Integer.parseInt(value) <= Config.DIFF_MAX_VERSION_AND_MAX_KEEP_VERSION
+        ? "0"
+        : String.valueOf(Integer.parseInt(value) - Config.DIFF_MAX_VERSION_AND_MAX_KEEP_VERSION);
+    }
+    return DataFetcherResult
+      .<Map<String, String>>newResult()
+      .data(resultMap)
+      .build();
   }
 }
