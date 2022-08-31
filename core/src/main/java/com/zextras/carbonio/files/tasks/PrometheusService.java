0ab1f57bd358a8690360e557e9509fbc1c3d43e3
Domenico Stragliotto <domenico.stragliotto@zextras.com>
2022-08-31 15:26:59 +0200

    change hcl to fix metrics endpoint expose

--- core/src/main/java/com/zextras/carbonio/files/tasks/PrometheusService.java
+++ core/src/main/java/com/zextras/carbonio/files/tasks/PrometheusService.java
@@ -1,5 +1,6 @@
 package com.zextras.carbonio.files.tasks;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.prometheus.PrometheusConfig;
@@ -8,9 +9,10 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 @Singleton
 public class PrometheusService {
 
-  private static final PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
-  private static final Counter uploadCounter = prometheusRegistry.counter("files.upload","files");
-  private static final Counter uploadVersionCounter = prometheusRegistry.counter("files.upload.version","files","version");
+  private final PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+  private final Counter uploadCounter = prometheusRegistry.counter("files.upload","service","files","uri","/upload");
+  private final Counter uploadVersionCounter = prometheusRegistry.counter("files.upload","service","files","uri","/upload/version");
+
 
   public PrometheusMeterRegistry getRegistry(){
     return prometheusRegistry;
