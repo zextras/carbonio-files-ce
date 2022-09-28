24283d5d1ee2e804dd365c93d6e43ae8dfde11d0
Federico Rispo <44871614+federicorispo@users.noreply.github.com>
2022-09-28 14:31:46 +0200

    fix: FILES-436 - Fix GetPath API to return nodes ordered correctly (#41)
    When the getPath API is called, now it returns the nodes in a correct order.

--- core/src/main/java/com/zextras/carbonio/files/tasks/PrometheusService.java
+++ core/src/main/java/com/zextras/carbonio/files/tasks/PrometheusService.java
@@ -1,6 +1,9 @@
+// SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 package com.zextras.carbonio.files.tasks;
 
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.prometheus.PrometheusConfig;
