258214c673902106a66a8c34a569eaf47fe8320d
Federico Rispo <federico.rispo@zextras.com>
2023-11-11 15:34:50 +0100

    chore(format): reformat ShareRepository and ShareRepositoryEbean class

--- core/src/main/java/com/zextras/carbonio/files/dal/repositories/impl/ebean/ShareRepositoryEbean.java
+++ core/src/main/java/com/zextras/carbonio/files/dal/repositories/impl/ebean/ShareRepositoryEbean.java
@@ -32,41 +32,33 @@ public class ShareRepositoryEbean implements ShareRepository {
    *
    * @param nodeId is a {@link String} representing the id of the node on the requested share.
    * @param userId is a {@link String} representing the id of the user on the requested share.
-   *
    * @return an {@link Optional} of {@link Share} if it exists, an {@link Optional#empty()}
-   * otherwise.
+   *     otherwise.
    */
-  private Optional<Share> getRealShare(
-    String nodeId,
-    String userId
-  ) {
+  private Optional<Share> getRealShare(String nodeId, String userId) {
     return mDB.getEbeanDatabase()
-      .find(Share.class)
-      .where()
-      .eq(Files.Db.Share.NODE_ID, nodeId)
-      .eq(Files.Db.Share.SHARE_TARGET_UUID, userId)
-      .findOneOrEmpty();
+        .find(Share.class)
+        .where()
+        .eq(Files.Db.Share.NODE_ID, nodeId)
+        .eq(Files.Db.Share.SHARE_TARGET_UUID, userId)
+        .findOneOrEmpty();
   }
 
-  public Optional<Share> getShare(
-    String nodeId,
-    String userId
-  ) {
+  public Optional<Share> getShare(String nodeId, String userId) {
     return getRealShare(nodeId, userId);
   }
 
   public Optional<Share> upsertShare(
-    String nodeId,
-    String targetUserId,
-    ACL permissions,
-    Boolean direct,
-    Boolean createdViaCollaborationLink,
-    Optional<Long> expireTimestamp
-  ) {
+      String nodeId,
+      String targetUserId,
+      ACL permissions,
+      Boolean direct,
+      Boolean createdViaCollaborationLink,
+      Optional<Long> expireTimestamp) {
     Optional<Share> targetShare = getShare(nodeId, targetUserId);
     if (targetShare.isPresent()) {
       if (!targetShare.get().isDirect()) {
-        //We're converting an inherited share to a direct share
+        // We're converting an inherited share to a direct share
         Share updateShare = targetShare.get();
         if (direct) {
           updateShare.setDirect(true);
@@ -79,15 +71,15 @@ public class ShareRepositoryEbean implements ShareRepository {
         return Optional.empty();
       }
     } else {
-      final Share share = new Share(
-        nodeId,
-        targetUserId,
-        permissions,
-        System.currentTimeMillis(),
-        direct,
-        createdViaCollaborationLink,
-        null
-      );
+      final Share share =
+          new Share(
+              nodeId,
+              targetUserId,
+              permissions,
+              System.currentTimeMillis(),
+              direct,
+              createdViaCollaborationLink,
+              null);
       expireTimestamp.ifPresent(share::setExpiredAt);
       mDB.getEbeanDatabase().save(share);
       return Optional.of(share);
@@ -95,13 +87,12 @@ public class ShareRepositoryEbean implements ShareRepository {
   }
 
   public void upsertShareBulk(
-    List<String> nodeIds,
-    String targetUserId,
-    ACL permissions,
-    Boolean direct,
-    Boolean createdViaCollaborationLink,
-    Optional<Long> expireTimestamp
-  ) {
+      List<String> nodeIds,
+      String targetUserId,
+      ACL permissions,
+      Boolean direct,
+      Boolean createdViaCollaborationLink,
+      Optional<Long> expireTimestamp) {
     try (Transaction transaction = mDB.getEbeanDatabase().beginTransaction()) {
 
       // use JDBC batch
@@ -109,16 +100,15 @@ public class ShareRepositoryEbean implements ShareRepository {
       transaction.setBatchSize(50);
 
       // these go to batch buffer
-      nodeIds.forEach(nodeId ->
-        upsertShare(
-          nodeId,
-          targetUserId,
-          permissions,
-          direct,
-          createdViaCollaborationLink,
-          expireTimestamp
-        )
-      );
+      nodeIds.forEach(
+          nodeId ->
+              upsertShare(
+                  nodeId,
+                  targetUserId,
+                  permissions,
+                  direct,
+                  createdViaCollaborationLink,
+                  expireTimestamp));
       // flush batch and commit
       transaction.commit();
     }
@@ -129,19 +119,13 @@ public class ShareRepositoryEbean implements ShareRepository {
     return share;
   }
 
-  public boolean deleteShare(
-    String nodeId,
-    String targetUserId
-  ) {
+  public boolean deleteShare(String nodeId, String targetUserId) {
     return getShare(nodeId, targetUserId)
-      .map(share -> mDB.getEbeanDatabase().delete(share))
-      .orElse(false);
+        .map(share -> mDB.getEbeanDatabase().delete(share))
+        .orElse(false);
   }
 
-  public void deleteSharesBulk(
-    List<String> nodeIds,
-    String targetUserId
-  ) {
+  public void deleteSharesBulk(List<String> nodeIds, String targetUserId) {
     try (Transaction transaction = mDB.getEbeanDatabase().beginTransaction()) {
 
       // use JDBC batch
@@ -156,39 +140,24 @@ public class ShareRepositoryEbean implements ShareRepository {
     }
   }
 
-  public void deleteSharesBulk(
-    List<String> nodeIds
-  ) {
-    mDB.getEbeanDatabase()
-      .find(Share.class)
-      .where()
-      .in(Files.Db.Share.NODE_ID, nodeIds)
-      .delete();
+  public void deleteSharesBulk(List<String> nodeIds) {
+    mDB.getEbeanDatabase().find(Share.class).where().in(Files.Db.Share.NODE_ID, nodeIds).delete();
 
     // TODO Uniform the delete behaviour with other delete method when we implement unique shareId
   }
 
-  public List<Share> getShares(
-    List<String> nodeIds,
-    String targetUserId
-  ) {
+  public List<Share> getShares(List<String> nodeIds, String targetUserId) {
     return mDB.getEbeanDatabase()
-      .find(Share.class)
-      .where()
-      .in(Files.Db.Share.NODE_ID, nodeIds)
-      .eq(Files.Db.Share.SHARE_TARGET_UUID, targetUserId)
-      .findList();
+        .find(Share.class)
+        .where()
+        .in(Files.Db.Share.NODE_ID, nodeIds)
+        .eq(Files.Db.Share.SHARE_TARGET_UUID, targetUserId)
+        .findList();
   }
 
-  public List<Share> getShares(
-    String nodeId,
-    List<String> targetUserIds
-  ) {
-    Query<Share> query = mDB.getEbeanDatabase()
-      .find(Share.class)
-      .where()
-      .eq(Files.Db.Share.NODE_ID, nodeId)
-      .query();
+  public List<Share> getShares(String nodeId, List<String> targetUserIds) {
+    Query<Share> query =
+        mDB.getEbeanDatabase().find(Share.class).where().eq(Files.Db.Share.NODE_ID, nodeId).query();
 
     if (!targetUserIds.isEmpty()) {
       query.where().in(Files.Db.Share.SHARE_TARGET_UUID, targetUserIds);
@@ -199,26 +168,21 @@ public class ShareRepositoryEbean implements ShareRepository {
 
   public List<Share> getShares(List<String> nodeIds) {
     return mDB.getEbeanDatabase()
-      .find(Share.class)
-      .where()
-      .in(Db.Share.NODE_ID, nodeIds)
-      .findList();
+        .find(Share.class)
+        .where()
+        .in(Db.Share.NODE_ID, nodeIds)
+        .findList();
   }
 
-  public List<String> getSharesUsersIds(
-    String nodeId,
-    List<ShareSort> sorts
-  ) {
-    Query<Share> query = mDB.getEbeanDatabase()
-      .createQuery(Share.class)
-      .where()
-      .eq(Files.Db.Share.NODE_ID, nodeId)
-      .query();
+  public List<String> getSharesUsersIds(String nodeId, List<ShareSort> sorts) {
+    Query<Share> query =
+        mDB.getEbeanDatabase()
+            .createQuery(Share.class)
+            .where()
+            .eq(Files.Db.Share.NODE_ID, nodeId)
+            .query();
 
     sorts.forEach(sort -> sort.getOrderEbeanQuery(query));
-    return query.findList()
-      .stream()
-      .map(Share::getTargetUserId)
-      .collect(Collectors.toList());
+    return query.findList().stream().map(Share::getTargetUserId).collect(Collectors.toList());
   }
 }
