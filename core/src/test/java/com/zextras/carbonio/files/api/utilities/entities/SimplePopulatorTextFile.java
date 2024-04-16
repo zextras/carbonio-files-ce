f664707e13c68e11f7fdef0898caaf9fc283f83a
matteogalvagni <matteo.galvagni@zextras.com>
2024-04-16 12:04:44 +0200

    fix: fixed headers

--- core/src/test/java/com/zextras/carbonio/files/api/utilities/entities/SimplePopulatorTextFile.java
+++ core/src/test/java/com/zextras/carbonio/files/api/utilities/entities/SimplePopulatorTextFile.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 package com.zextras.carbonio.files.api.utilities.entities;
 
 import com.zextras.carbonio.files.dal.dao.ebean.NodeType;
