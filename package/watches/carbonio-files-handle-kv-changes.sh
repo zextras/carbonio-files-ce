ed8da76bccc129b11fdd1670c6f02ae033dc4ee9
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-07-11 17:03:54 +0200

    feat: add message broker call in script

--- package/watches/carbonio-files-handle-kv-changes.sh
+++ package/watches/carbonio-files-handle-kv-changes.sh
@@ -10,3 +10,20 @@ KEY=$(echo $INPUT | jq -r '.Key')
 VALUE=$(echo $INPUT | jq -r '.Value' | base64 --decode)
 
 echo "La chiave $KEY è stata modificata. Il nuovo valore è $VALUE."
+
+USERNAME=$(consul kv get -token-file=/etc/carbonio/files/service-discover/token carbonio-message-bro>
+PASSWORD=$(consul kv get -token-file=/etc/carbonio/files/service-discover/token carbonio-message-bro>
+
+BODY=$(jq -n --arg key "$KEY" --arg value "$VALUE" '{($key): $value}')
+echo "$BODY"
+
+curl --http0.9 -u $USERNAME:$PASSWORD -X POST -H "Content-Type: application/json" \
+-d "$BODY" http://127.78.0.2:20006/api/exchanges/%2f/TEST/publish \
+-d '{
+    "properties": {
+        "delivery_mode": 2
+    },
+    "routing_key": "",
+    "payload": "'"$BODY"'",
+    "payload_encoding": "string"
+}'
