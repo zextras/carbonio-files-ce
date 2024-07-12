2a57a52ba54d7fa2e07d1d1480071dca8f648c12
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-07-12 13:34:05 +0200

    feat: add script that publishes event on kv change

---
+++ package/watches/carbonio-files-handle-kv-changes.py
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+import json
+import base64
+import sys
+import subprocess
+import pika
+
+
+input_str = sys.stdin.read()
+input_json = json.loads(input_str)
+
+key = input_json['Key']
+value = base64.b64decode(input_json['Value']).decode()
+
+print(f"Key {key} has been changed into {value}.")
+
+username = subprocess.run(["consul", "kv", "get", "-token-file=/etc/carbonio/files/service-discover/token", "carbonio-message-broker/username"], capture_output=True, text=True)
+password = subprocess.run(["consul", "kv", "get", "-token-file=/etc/carbonio/files/service-discover/token", "carbonio-message-broker/password"], capture_output=True, text=True)
+
+username = username.stdout.strip()
+password = password.stdout.strip()
+
+credentials = pika.PlainCredentials(username, password)
+parameters = pika.ConnectionParameters('127.78.0.2', 20006, '/', credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+exchange_name = 'KV_CHANGED_EXCHANGE'
+channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
+
+print(f"Declared '{exchange_name}'")
+
+message = json.dumps({key: value})
+channel.basic_publish(
+    exchange=exchange_name,
+    routing_key='',
+    body=message,
+    properties=pika.BasicProperties(
+        delivery_mode=2,
+    )
+)
+
+print(f"Published message on '{exchange_name}'")
+
+connection.close()
--- package/watches/carbonio-files-start-watches.sh
+++ package/watches/carbonio-files-start-watches.sh
@@ -5,4 +5,4 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 # Here we start all watchers instead of having a systemd service for each kv
-consul watch -type=key -key=carbonio-files/max-number-of-versions -token-file=/etc/carbonio/files/service-discover/token /usr/bin/carbonio-files-handle-kv-changes.sh
+consul watch -type=key -key=carbonio-files/max-number-of-versions -token-file=/etc/carbonio/files/service-discover/token "python3 /usr/bin/carbonio-files-handle-kv-changes.py"
