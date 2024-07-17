b925183ab13ba91222aaa95c4791c3916b931ba9
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-07-17 12:39:37 +0200

    fix: try update rabbitmq image

--- core/src/test/java/com/zextras/carbonio/files/Simulator.java
+++ core/src/test/java/com/zextras/carbonio/files/Simulator.java
@@ -24,7 +24,6 @@ import com.zextras.carbonio.usermanagement.enumerations.UserType;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.http.HttpMethod;
 import java.nio.charset.StandardCharsets;
-import java.time.Duration;
 import java.util.Map;
 import java.util.Properties;
 import org.mockserver.client.MockServerClient;
@@ -37,13 +36,10 @@ import org.mockserver.model.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.com.trilead.ssh2.crypto.Base64;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
-
-import static java.time.temporal.ChronoUnit.SECONDS;
 
 @Testcontainers
 public class Simulator implements AutoCloseable {
@@ -85,11 +81,7 @@ public class Simulator implements AutoCloseable {
 
   private Simulator startMessageBroker() {
     if (messageBrokerContainer == null) {
-      messageBrokerContainer = new RabbitMQContainer("rabbitmq:3.7.25-management-alpine");
-      messageBrokerContainer.setWaitStrategy(new LogMessageWaitStrategy()
-          .withRegEx(".*Server startup complete.*")
-          .withTimes(1)
-          .withStartupTimeout(Duration.of(180, SECONDS)));
+      messageBrokerContainer = new RabbitMQContainer("rabbitmq:3.13.4");
     }
     messageBrokerContainer.start();
 
