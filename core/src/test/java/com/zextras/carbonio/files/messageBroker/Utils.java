cc4483e679d817c846eb0c68264a8abed08e629c
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-07-18 14:49:59 +0200

    fix: add license

--- core/src/test/java/com/zextras/carbonio/files/messageBroker/Utils.java
+++ core/src/test/java/com/zextras/carbonio/files/messageBroker/Utils.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 package com.zextras.carbonio.files.messageBroker;
 
 import java.util.function.Supplier;
