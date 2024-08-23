e2742a33c13fb63c94972394e0de8537c77449bb
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-08-23 14:21:44 +0200

    feat: add user deleted consumer

---
+++ core/src/main/java/com/zextras/carbonio/files/message_broker/consumers/UserDeletedConsumer.java
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.files.message_broker.consumers;
+import com.google.inject.Inject;
+import com.zextras.carbonio.files.dal.repositories.interfaces.NodeRepository;
+import com.zextras.carbonio.message_broker.config.EventConfig;
+import com.zextras.carbonio.message_broker.consumer.BaseConsumer;
+import com.zextras.carbonio.message_broker.events.generic.BaseEvent;
+import com.zextras.carbonio.message_broker.events.services.mailbox.UserDeleted;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserDeletedConsumer extends BaseConsumer {
+
+  private static final Logger logger = LoggerFactory.getLogger(UserDeletedConsumer.class);
+
+  private final NodeRepository nodeRepository;
+
+  @Inject
+  public UserDeletedConsumer(NodeRepository nodeRepository) {
+    this.nodeRepository = nodeRepository;
+  }
+
+  @Override
+  protected EventConfig getEventConfig() {
+    return EventConfig.USER_DELETED;
+  }
+
+  @Override
+  public void doHandle(BaseEvent baseMessageBrokerEvent) {
+    UserDeleted userDeleted = (UserDeleted) baseMessageBrokerEvent;
+    logger.info("Received UserDeleted({})", userDeleted.getUserId());
+  }
+}
