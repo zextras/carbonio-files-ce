6e8cbb21a7001c0fb1e78bf7b4ad731f44a45701
galvagnimatteo <galvagni.matteo@protonmail.com>
2025-01-14 11:08:38 +0100

    feat: add db migration for mimetypes

--- core/src/main/resources/sql/postgresql_7.sql
+++ core/src/main/resources/sql/postgresql_7.sql
@@ -4,8 +4,22 @@
 
 BEGIN;
 
-/* Replace all nodetypes from OTHER to MESSAGE for all nodes with mimetype message/<something> */
-/* Replace mimetypes on revision table from application/octet-stream to application/vnd.ms-outlook for files with .msg extension
-and replace their nodetypes with MESSAGE */
+/* Replace all nodetypes from OTHER to MESSAGE for all nodes with extensions related to messages */
+UPDATE node
+SET node_type = 'MESSAGE'
+WHERE node_type = 'OTHER'
+AND (
+  RIGHT(name, LENGTH(name) - POSITION('.' IN REVERSE(name))) IN ('u8msg', 'u8dsn', 'u8mdn', 'u8hdr', 'eml', 'mail', 'art', 'msg')
+);
+
+/* Replace mimetypes on revision table from application/octet-stream to application/vnd.ms-outlook for files with .msg extension */
+UPDATE revision
+SET mime_type = 'application/vnd.ms-outlook'
+FROM node
+WHERE revision.node_id = node.node_id
+AND revision.mime_type = 'application/octet-stream'
+AND RIGHT(node.name, 4) = '.msg';
+
+UPDATE db_info SET version = 7;
 
 COMMIT;
