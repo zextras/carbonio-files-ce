92bbbc77ccf280bd93834fcc423312887eb186cf
Federico Rispo <federico.rispo@zextras.com>
2025-01-15 16:15:59 +0100

    test: add DbMigration7Test to verify the correctness of the db migration (#153)
    - Added DbMigration7Test to verify the postgresql_7 migration script
    - Bumped postgreSql container version to 16.6 (so it is align with the version in production)
    - Updated the test/postgresql script with the latest migration (spoiler: it remains commented)
    
    Refs: FILES-666

--- core/src/main/resources/sql/postgresql_7.sql
+++ core/src/main/resources/sql/postgresql_7.sql
@@ -1,16 +1,17 @@
--- SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+-- SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
 --
 -- SPDX-License-Identifier: AGPL-3.0-only
 
 BEGIN;
 
-/* Replace all nodetypes from OTHER to MESSAGE for all nodes with extensions related to messages */
+-- Replace all node_types from OTHER to MESSAGE for all nodes with extensions related to messages
 UPDATE node
 SET node_type = 'MESSAGE'
 WHERE node_type = 'OTHER'
 AND split_part(name, '.', -1) IN ('u8msg', 'u8dsn', 'u8mdn', 'u8hdr', 'eml', 'mail', 'art', 'msg');
 
-/* Replace mimetypes on revision table from application/octet-stream to application/vnd.ms-outlook for files with .msg extension */
+-- Replace mimetypes on revision table from application/octet-stream to application/vnd.ms-outlook for
+-- files with .msg extension
 UPDATE revision
 SET mime_type = 'application/vnd.ms-outlook'
 FROM node
