46bfa771a7fca794924efd11312c761624bcec18
galvagnimatteo <galvagni.matteo@protonmail.com>
2025-08-05 08:59:36 +0200

    fix: add extension to zip name

--- core/src/main/java/com/zextras/carbonio/files/rest/services/BlobService.java
+++ core/src/main/java/com/zextras/carbonio/files/rest/services/BlobService.java
@@ -707,7 +707,7 @@ public class BlobService {
 
     String zipName = "Files.zip";
     if (nodes.size() == 1) {
-      zipName = nodes.get(0).getName();
+      zipName = nodes.get(0).getName() + ".zip";
     }
 
     try {
