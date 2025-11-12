6a09703f3fab9d8169023839355f1feba76174dd
Matteo Baglini <matteo.baglini@gmail.com>
2025-11-12 11:57:12 +0100

    feat: read hikari idle timeout, leak detection and lifetime configuration values

--- core/src/main/java/com/zextras/carbonio/files/config/FilesConfig.java
+++ core/src/main/java/com/zextras/carbonio/files/config/FilesConfig.java
@@ -236,6 +236,27 @@ public class FilesConfig {
         .getOrElse(Constants.Config.Hikari.MIN_IDLE_CONNECTIONS);
   }
 
+  public int getHikariIdleTimeout() {
+    return ServiceDiscoverHttpClient.defaultURL(ServiceDiscover.SERVICE_NAME)
+      .getConfig(ServiceDiscover.Config.Key.HIKARI_IDLE_TIMEOUT)
+      .map(Integer::parseInt)
+      .getOrElse(Constants.Config.Hikari.IDLE_TIMEOUT);
+  }
+
+  public int getHikariLeakDetectionThreshold() {
+    return ServiceDiscoverHttpClient.defaultURL(ServiceDiscover.SERVICE_NAME)
+      .getConfig(ServiceDiscover.Config.Key.HIKARI_LEAK_DETECTION_THRESHOLD)
+      .map(Integer::parseInt)
+      .getOrElse(Constants.Config.Hikari.LEAK_DETECTION_THRESHOLD);
+  }
+
+  public int getHikariMaxLifetime() {
+    return ServiceDiscoverHttpClient.defaultURL(ServiceDiscover.SERVICE_NAME)
+      .getConfig(ServiceDiscover.Config.Key.HIKARI_MAX_LIFETIME)
+      .map(Integer::parseInt)
+      .getOrElse(Constants.Config.Hikari.MAX_LIFETIME);
+  }
+
   // ================================================================================
   // Application Configuration Methods (non-connectivity related)
   // ================================================================================
